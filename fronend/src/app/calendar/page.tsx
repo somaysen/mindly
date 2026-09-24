1ed65fac764ea2eec8b263ca2e0bcf26822e20a5
Somaysen <somaysen656@gmail.com>
2026-09-24 10:57:09 +0530

    feat(calendar): add Bryntum calendar view

---
+++ fronend/src/app/calendar/page.tsx
@@ -0,0 +1,9 @@
+import { BryntumCalendarView } from "@/features/calendar";
+
+export default function CalendarPage() {
+  return (
+    <main className="min-h-screen bg-[#0c0b19] p-6 text-white">
+      <BryntumCalendarView />
+    </main>
+  );
+}
